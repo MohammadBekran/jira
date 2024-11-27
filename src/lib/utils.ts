e76571018ca58b99eb2e44908ef6455686c69477
Mohammad <bhshtbaran@gmail.com>
2024-11-27 07:41:51 -0800

    feat: implement invite code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,4 +6,16 @@ const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
 
-export { cn, toast };
+const generateInviteCode = (length: number) => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcsefghijklmnoqrstuvwxyz0123456789";
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  return result;
+};
+
+export { cn, generateInviteCode, toast };
